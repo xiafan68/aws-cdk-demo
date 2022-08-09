a007e9685d07ef720de2cd64853a480f5abdc451
夏帆 <xiafan.68@bytedance.com>
2022-08-09 17:35:27 +0800

    feat: add pipeline demo

--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -1,5 +1,6 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps,RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkDemoStack extends Stack {
@@ -7,7 +8,26 @@ export class CdkDemoStack extends Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true
+    });
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkDemoQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
 
+export class AnotherCdkDemoStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
     // example resource
     // const queue = new sqs.Queue(this, 'CdkDemoQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
---
+++ lib/cdk-pipeline-demo.ts
@@ -0,0 +1,29 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { CdkDemoStack } from './cdk-demo-stack';
+
+export class MyPipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'MyPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('xiafan68/aws-cdk-demo', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
+    });
+    pipeline.addStage(new MyPipelineAppStage(this, "test", {}))
+    pipeline.addStage(new MyPipelineAppStage(this, "test-new", {}))
+  }
+}
+
+export class MyPipelineAppStage extends cdk.Stage {
+    
+    constructor(scope: Construct, id: string, props?: cdk.StageProps) {
+      super(scope, id, props);
+  
+      const lambdaStack = new CdkDemoStack(this, 'LambdaStack');      
+    }
+}
